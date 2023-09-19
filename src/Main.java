4c1a885d7dd92f30c26ef41f8e64671c0d4fd0b5
Buryak Elena <ufh2013br@gmail.com>
2023-09-19 14:08:00 +0700

    srs

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2_000;
+        int mils = 20;
+        int bonus = ticket / mils;
+
+        System.out.println("bonus = " + bonus);
+    }
+}
